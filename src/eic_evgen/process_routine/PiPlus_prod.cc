91d832c02e1c1612c01f5ec6d1ec6bbe96067425
Stephen Kay <sjdkay@gmail.com>
2021-06-24 16:00:26 -0600

    Modified output of generated events. Weight should now be readable by EIC_Smear

--- src/eic_evgen/process_routine/PiPlus_prod.cc
+++ src/eic_evgen/process_routine/PiPlus_prod.cc
@@ -535,8 +535,9 @@ void PiPlus_Production::Processing_Event() {
   //             Lab cross section     Phase Space   Conversion     Luminosity                Total events tried
   // Hz        = ub / ( sr^2 * GeV ) * GeV * sr^2 * ( cm^2 / ub ) * ( # / ( cm^2 * sec ) ) / ( # )
 
-  // SJDK 11/05/21 -  This is the previous non unit weight
-  fEventWeight = fSigma_Col * fPSF * fuBcm2 * fLumi / fNEvents;   // in Hz
+  // SJDK 11/05/21 - This is the previous non unit weight
+  // SJDK 24/06/21 - Explicitly taking the absolute value of the weigth such that the value is positive!
+  fEventWeight = abs(fSigma_Col * fPSF * fuBcm2 * fLumi / fNEvents);   // in Hz
   // SJDK 21/06/21 - Commenting out "unit weight" calculation for now, reverting to old version
   // SJDK 11/05/21 - New weight calculation, division by ceiling weight value to attempt to get a "unit" value
   //fEventWeight = abs(fSigma_Col * fPSF * fuBcm2 * fLumi )/fEventWeightCeil;
@@ -913,9 +914,9 @@ void PiPlus_Production::PiPlus_Pythia6_Out_Init() {
 
 	print_itt = 0;
 
-	ppiOut << "SIMPLE Event FILE" << endl;
+	ppiOut << "DEMP Event FILE" << endl;
 	ppiOut << "============================================" << endl;
-	ppiOut << "I, ievent, nParticles" << endl;
+	ppiOut << "I, ievent, nParticles, Weight" << endl;
 	ppiOut << "============================================" << endl;
 	ppiOut << "I  K(I,1)  K(I,2)  K(I,3)  K(I,4)  K(I,5)  P(I,1)  P(I,2)  P(I,3)  P(I,4)  P(I,5)  V(I,1)  V(I,2)  V(I,3)" << endl;
 	ppiOut << "============================================" << endl;
@@ -938,124 +939,124 @@ void PiPlus_Production::PiPlus_Pythia6_Output() {
 // 	   << " \t " << fEventWeight   // var 8	   
 // 	   << endl;
 
-    ppiOut << "0" << " \t\t\t\ "  << print_itt << " \t\t\t " << "1" << endl;           // var 1
+  ppiOut << "0" << " \t\t\t "  << print_itt << " \t\t\t " << "1" << " \t\t\t " << fEventWeight << endl; // var 1
 
-	print_itt++;
+  print_itt++;
 
-	ppiOut << "============================================" << endl;
+  ppiOut << "============================================" << endl;
 
- 	///*--------------------------------------------------*/
-  	// Initial State
+  ///*--------------------------------------------------*/
+  // Initial State
  
-      ppiOut  << "1" 
-  	   << setw(6) << "21" 
-  	   << setw(6) << "11"
-  	   << setw(6) << "0" 
-  	   << setw(6) << "3" 
-  	   << setw(6) << "4" 
+  ppiOut  << "1" 
+	  << setw(6) << "21" 
+	  << setw(6) << "11"
+	  << setw(6) << "0" 
+	  << setw(6) << "3" 
+	  << setw(6) << "4" 
 
-  	   << setw(14) << r_lelectrong.X()
-  	   << setw(14) << r_lelectrong.Y()   
-  	   << setw(14) << r_lelectrong.Z()  
-  	   << setw(14) << r_lelectrong.E()
-  	   << setw(14) << fElectron_Mass_GeV
-  	   << setw(6) << fVertex_X
-  	   << setw(6) << fVertex_Y
-  	   << setw(6) << fVertex_Z
-  	   << endl;
+	  << setw(14) << r_lelectrong.X()
+	  << setw(14) << r_lelectrong.Y()   
+	  << setw(14) << r_lelectrong.Z()  
+	  << setw(14) << r_lelectrong.E()
+	  << setw(14) << fElectron_Mass_GeV
+	  << setw(6) << fVertex_X
+	  << setw(6) << fVertex_Y
+	  << setw(6) << fVertex_Z
+	  << endl;
 
-      ppiOut << "2" 
-  	   << setw(6) << "21" 
-  	   << setw(6) << "2212"
-  	   << setw(6) << "0" 
-  	   << setw(6) << "5" 
-  	   << setw(6) << "6" 
+  ppiOut << "2" 
+	 << setw(6) << "21" 
+	 << setw(6) << "2212"
+	 << setw(6) << "0" 
+	 << setw(6) << "5" 
+	 << setw(6) << "6" 
 
-  	   << setw(14) << r_lprotong.X()
-  	   << setw(14) << r_lprotong.Y()   
-  	   << setw(14) << r_lprotong.Z()  
-  	   << setw(14) << r_lprotong.E()
-  	   << setw(14) << fProton_Mass_GeV
-  	   << setw(6) << fVertex_X
-  	   << setw(6) << fVertex_Y
-  	   << setw(6) << fVertex_Z
-  	   << endl;
+	 << setw(14) << r_lprotong.X()
+	 << setw(14) << r_lprotong.Y()   
+	 << setw(14) << r_lprotong.Z()  
+	 << setw(14) << r_lprotong.E()
+	 << setw(14) << fProton_Mass_GeV
+	 << setw(6) << fVertex_X
+	 << setw(6) << fVertex_Y
+	 << setw(6) << fVertex_Z
+	 << endl;
 
-      ppiOut << "3" 
-  	   << setw(6) << "21" 
-  	   << setw(6) << "22"
-  	   << setw(6) << "1" 
-  	   << setw(6) << "0" 
-  	   << setw(6) << "0" 
+  ppiOut << "3" 
+	 << setw(6) << "21" 
+	 << setw(6) << "22"
+	 << setw(6) << "1" 
+	 << setw(6) << "0" 
+	 << setw(6) << "0" 
 
-  	   << setw(14) << r_lphotong.X()
-  	   << setw(14) << r_lphotong.Y()   
-  	   << setw(14) << r_lphotong.Z()  
-  	   << setw(14) << r_lphotong.E()
-  	   << setw(14) << r_lphotong.M()
-  	   << setw(6) << fVertex_X
-  	   << setw(6) << fVertex_Y
-  	   << setw(6) << fVertex_Z
-  	   << endl;
+	 << setw(14) << r_lphotong.X()
+	 << setw(14) << r_lphotong.Y()   
+	 << setw(14) << r_lphotong.Z()  
+	 << setw(14) << r_lphotong.E()
+	 << setw(14) << r_lphotong.M()
+	 << setw(6) << fVertex_X
+	 << setw(6) << fVertex_Y
+	 << setw(6) << fVertex_Z
+	 << endl;
 
 
- 	///*--------------------------------------------------*/
-  	// Final State
+  ///*--------------------------------------------------*/
+  // Final State
       
-      // Scattered electron
-      ppiOut << "4" 
-  	   << setw(6) << "1" 
-  	   << setw(6) << "11" 
-  	   << setw(6) << "1" 
-  	   << setw(6) << "0"
-  	   << setw(6) << "0"
+  // Scattered electron
+  ppiOut << "4" 
+	 << setw(6) << "1" 
+	 << setw(6) << "11" 
+	 << setw(6) << "1" 
+	 << setw(6) << "0"
+	 << setw(6) << "0"
  
-  	   << setw(14) << r_lscatelecg.X() 
-  	   << setw(14) << r_lscatelecg.Y() 
-  	   << setw(14) << r_lscatelecg.Z() 
-  	   << setw(14) << r_lscatelecg.E()
-  	   << setw(14) << fElectron_Mass_GeV
-  	   << setw(6) << fVertex_X
-  	   << setw(6) << fVertex_Y
-  	   << setw(6) << fVertex_Z
-  	   << endl;
+	 << setw(14) << r_lscatelecg.X() 
+	 << setw(14) << r_lscatelecg.Y() 
+	 << setw(14) << r_lscatelecg.Z() 
+	 << setw(14) << r_lscatelecg.E()
+	 << setw(14) << fElectron_Mass_GeV
+	 << setw(6) << fVertex_X
+	 << setw(6) << fVertex_Y
+	 << setw(6) << fVertex_Z
+	 << endl;
   	  
-      // Recoiled nucleon
-      ppiOut << "5" 
-  	   << setw(6) << "1" 
-  	   << setw(6) << PDGtype(recoil_nucleon)
-  	   << setw(6) << "2" 
-  	   << setw(6) << "0"
-  	   << setw(6) << "0"
+  // Recoiled nucleon
+  ppiOut << "5" 
+	 << setw(6) << "1" 
+	 << setw(6) << PDGtype(recoil_nucleon)
+	 << setw(6) << "2" 
+	 << setw(6) << "0"
+	 << setw(6) << "0"
  
-  	   << setw(14) << r_l_scat_nucleon_g.X() 
-  	   << setw(14) << r_l_scat_nucleon_g.Y()
-  	   << setw(14) << r_l_scat_nucleon_g.Z()
-  	   << setw(14) << r_l_scat_nucleon_g.E()
-  	   << setw(14) << f_Scat_Nucleon_Mass_GeV
-  	   << setw(6) << fVertex_X
-  	   << setw(6) << fVertex_Y
-  	   << setw(6) << fVertex_Z
-  	   << endl;
+	 << setw(14) << r_l_scat_nucleon_g.X() 
+	 << setw(14) << r_l_scat_nucleon_g.Y()
+	 << setw(14) << r_l_scat_nucleon_g.Z()
+	 << setw(14) << r_l_scat_nucleon_g.E()
+	 << setw(14) << f_Scat_Nucleon_Mass_GeV
+	 << setw(6) << fVertex_X
+	 << setw(6) << fVertex_Y
+	 << setw(6) << fVertex_Z
+	 << endl;
  
-      // Produced Particle X
-      ppiOut << "6" 
-  	   << setw(6) << "1" 
-  	   << setw(6) << PDGtype(produced_X)
-  	   << setw(6) << "2" 
-  	   << setw(6) << "0" 
-  	   << setw(6) << "0"
+  // Produced Particle X
+  ppiOut << "6" 
+	 << setw(6) << "1" 
+	 << setw(6) << PDGtype(produced_X)
+	 << setw(6) << "2" 
+	 << setw(6) << "0" 
+	 << setw(6) << "0"
 
-  	   << setw(14) << r_lX_g.X()
-  	   << setw(14) << r_lX_g.Y()   
-  	   << setw(14) << r_lX_g.Z()  
-  	   << setw(14) << r_lX_g.E()
-  	   << setw(14) << fX_Mass_GeV
-  	   << setw(6) << fVertex_X
-  	   << setw(6) << fVertex_Y
-  	   << setw(6) << fVertex_Z
-  	   << endl;
+	 << setw(14) << r_lX_g.X()
+	 << setw(14) << r_lX_g.Y()   
+	 << setw(14) << r_lX_g.Z()  
+	 << setw(14) << r_lX_g.E()
+	 << setw(14) << fX_Mass_GeV
+	 << setw(6) << fVertex_X
+	 << setw(6) << fVertex_Y
+	 << setw(6) << fVertex_Z
+	 << endl;
 
-	ppiOut << "=============== Event finished ===============" << endl;
+  ppiOut << "=============== Event finished ===============" << endl;
 
 }
